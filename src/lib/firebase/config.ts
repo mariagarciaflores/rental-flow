ec5abbb2c5d0daaced3ae00dc74995ceece69f12
María Griselda Garcia Flores <grissgarcia74@gmail.com>
2025-11-10 19:53:57 +0000

    Implemented a solid version of rental flow program

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCLfqz7y7wmsqsR_nLnk6GbAmxe8WryEAg",
-  authDomain: "studio-4697983154-6ba04.firebaseapp.com",
-  projectId: "studio-4697983154-6ba04",
-  storageBucket: "studio-4697983154-6ba04.firebasestorage.app",
-  messagingSenderId: "895863737077",
-  appId: "1:895863737077:web:ff85ba96a7148e56db93bc"
+
 };
